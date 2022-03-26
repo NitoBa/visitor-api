62ea5a08181c8785c0b4f9f0ea3c7ba0f0334756
Bruno Alves <bruno@sthorm.io>
2022-03-26 20:48:57 -0300

    improvement: improvement error message

--- src/domain/errors/invalidParam.ts
+++ src/domain/errors/invalidParam.ts
@@ -2,7 +2,7 @@ import { DomainError } from './domainError'
 
 export class InvalidParamError extends Error implements DomainError {
   constructor (public readonly param: string) {
-    super(`Error: ${param} is invalid`)
+    super(`Error: the param "${param}" is invalid`)
     this.name = 'InvalidParamError'
   }
 }
