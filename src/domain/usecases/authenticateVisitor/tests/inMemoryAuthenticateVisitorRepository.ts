ad31f10b77eae489748d9c4bb71870a9b619bd3d
Bruno Alves <bruno@sthorm.io>
2022-03-27 10:36:51 -0300

    refactor: remove in memory authenticate repository

--- src/domain/usecases/authenticateVisitor/tests/inMemoryAuthenticateVisitorRepository.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/semi */
-import { IAuthenticateVisitorRepository } from '../../../repositories';
-
-export class AuthenticateVisitorRepositorySpy implements IAuthenticateVisitorRepository {
-  async authenticate (input: { email: string, password: string }): Promise<void> {
-
-  }
-}
