e938d0b7ebc82cdd9eb5cf01d4e3144948cde955
Bruno Alves <brukum2@gmail.com>
2022-03-27 01:42:48 -0300

    chore: change imports on invalid param error file

---
+++ src/shared/errors/invalidParam.ts
@@ -0,0 +1,8 @@
+import { DomainError } from '../../domain/errors/domainError'
+
+export class InvalidParamError extends Error implements DomainError {
+  constructor (public readonly param: string) {
+    super(`Error: the param "${param}" is invalid`)
+    this.name = 'InvalidParamError'
+  }
+}
