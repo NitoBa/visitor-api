f820d13069eb371a30c74ec47adab050e019a2af
Bruno Alves <bruno@sthorm.io>
2022-03-27 14:38:27 -0300

    refactor: validate hour method

--- src/domain/valueObjects/hour.ts
+++ src/domain/valueObjects/hour.ts
@@ -5,10 +5,17 @@ export class Hour {
   private constructor (public readonly hour: number) {}
 
   static create (hour: number): Either<InvalidParamError, Hour> {
-    if (hour < 0 || hour > 23) {
+    if (!Hour.validate(hour)) {
       return left(new InvalidParamError(hour.toString()))
     }
-
     return right(new Hour(hour))
+  }
+
+  static validate (hour: number): boolean {
+    if (hour === undefined || hour === null) return false
+
+    if (hour < 0 || hour > 23) return false
+
+    return true
   }
 }
--- src/domain/valueObjects/tests/hour-validator.spec.ts
+++ src/domain/valueObjects/tests/hour-validator.spec.ts
@@ -2,17 +2,17 @@ import { Hour } from '../hour'
 
 describe('Hour Validator', () => {
   it('should return an error if the hour is increase 23', () => {
-    const sut = Hour.create(24)
-    expect(sut.isLeft()).toBeTruthy()
+    const sut = Hour.validate(24)
+    expect(sut).toBeFalsy()
   })
 
   it('should return an error if the hour is smaller than 0', () => {
-    const sut = Hour.create(-1)
-    expect(sut.isLeft()).toBeTruthy()
+    const sut = Hour.validate(-1)
+    expect(sut).toBeFalsy()
   })
 
   it('should return a valid hour', () => {
-    const sut = Hour.create(14)
-    expect(sut.isRight()).toBeTruthy()
+    const sut = Hour.validate(14)
+    expect(sut).toBeTruthy()
   })
 })
