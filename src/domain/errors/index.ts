4a4dad1e86fb2e997880cdb5e8e409e9448e2963
Bruno Alves <brukum2@gmail.com>
2022-03-27 01:41:59 -0300

    chore: move invalid and missing param errors to shared folder

--- src/domain/errors/index.ts
+++ src/domain/errors/index.ts
@@ -1,5 +1,2 @@
-/* eslint-disable @typescript-eslint/semi */
-/* eslint-disable no-multiple-empty-lines */
-export * from './invalidParam';
-export * from './missingParams';
+
 
--- src/domain/errors/invalidParam.ts
+++
@@ -1,8 +0,0 @@
-import { DomainError } from './domainError'
-
-export class InvalidParamError extends Error implements DomainError {
-  constructor (public readonly param: string) {
-    super(`Error: the param "${param}" is invalid`)
-    this.name = 'InvalidParamError'
-  }
-}
