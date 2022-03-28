d65519a5a9d9787e22439d3736b7020ee980d14a
Bruno Alves <bruno@sthorm.io>
2022-03-28 10:26:32 -0300

    refactor: change name of folder from scheduling to appointment

--- src/domain/entities/index.ts
+++ src/domain/entities/index.ts
@@ -1,8 +1,8 @@
 /* eslint-disable no-multiple-empty-lines */
+export * from './appointment/appointment'
+export * from './appointment/appointmentData'
 export * from './establishment/establishment'
 export * from './establishment/establishmentData'
-export * from './scheduling/appointment'
-export * from './scheduling/appointmentData'
 export * from './visitor/visitor'
 export * from './visitor/visitorData'
 
